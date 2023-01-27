029bb5d997bb3c0c55934992052baf931b71f3c6
Laurie Fish <iamlauriefish@gmail.com>
2023-01-26 17:31:31 -0800

    Display and Post functions

--- app.js
+++ app.js
@@ -1,10 +1,54 @@
 /* Imports */
+import { getPosts } from './fetch.utils.js';
 
 /* Get DOM Elements */
 
 /* State */
+let postsData = [];
 
 /* Events */
+
+//user should be able to see a list of posts on load
+
+window.addEventListener('load', async () => {
+    const posts = await getPosts();
+
+    postsData = posts;
+    displayPosts();
+
+    
+
+function displayPosts() {
+    for (let post of postsData) {
+        const postEl = document.createElement('div');
+        const postTitleEl = document.createElement('p');
+        const postInfoEl = document.createElement('p');
+
+        postInfoEl.textContent = post.info;
+        postTitleEl.textContent = post.title;
+        postEl.append('postTitle', 'postInfo');
+
+        postEl.classList.add();
+        postTitleEl.classList.add;
+        postInfoEl.classList.add;
+        postEl.textContent = '';
+
+        return postEl;
+    }
+};
+
+}
+//on load check if user is logged in and if So redirect them to home pageXOffset.toExponential
+
+//Submit the sign in Form.
+//get the email and PW from the form
+//call signIn auth function with email and PW
+//redirect to home page.
+
+//submit sign up form
+//get the email and PW from the form
+//call signUp auth function with email and PW
+//redirect to home page.
 
 /* Display Functions */
 
---
+++ fetch.utils.js
@@ -0,0 +1,41 @@
+const SUPABASE_URL = 'https://jmdfixempqwyhkrrtcbj.supabase.co';
+const SUPABASE_KEY =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImptZGZpeGVtcHF3eWhrcnJ0Y2JqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzQ1NzUzOTYsImV4cCI6MTk5MDE1MTM5Nn0.6kedsKVXsFKvx_P4MONEnWJ_b7rHQWInoK8x8eST7_w';
+
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export async function getPosts() {
+    const { data, error } = await client.from('board').select('*');
+    console.log(data);
+    if (error) console.error(error);
+
+    return data;
+}
+
+export async function signIn(email, password) {
+    const { data } = await client.auth.signIn({
+        email: email,
+        password: password,
+    });
+}
+
+export async function signUp(email, password) {
+    const { data } = await client.auth.signUp({
+        email: email,
+        password: password,
+    });
+}
+
+export async function checkAuth() {
+    const user = await getUser();
+
+    if (!user) location.replace('/auth');
+}
+
+export async function redirectIfLoggedIn() {}
+
+export async function signOut() {
+    const response = await client.auth.signOut();
+
+    return response.error;
+}
